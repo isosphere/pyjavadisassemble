dd64bbf9db22fb4dac8c7598ae0502c007af237f
Matthew Scheffel <matt@weeoak.com>
2013-10-30 23:51:35 -0400

    Less debugging nonsense, command-line option

--- pyjavadisassemble.py
+++ pyjavadisassemble.py
@@ -5,9 +5,13 @@ import pprint # debugging
 import struct # unpacking binary data
 import binascii
 import prettytable
+import argparse
+
+parser = argparse.ArgumentParser(description='Disassemble a class file')
+parser.add_argument('--class', help='class file to disassemble', dest="classfile", required=True)
+args = parser.parse_args()
 
 # Load opcodes
-
 opcodes = {} # '0a' => { "name" => 'string', arguments => ["1: rawr", "2: blah"], stack => 'string', description => 'string' } 
 
 csvfile = open('opcodes.csv', 'r')
@@ -87,7 +91,6 @@ tag_structure = {
 }
 
 constant_pool = []
-classfile = open('NLicenseManager.class', 'rb')
 
 def Utf8Dereference(index):
     # Not a string, but a reference to one
@@ -100,136 +103,140 @@ def Utf8Dereference(index):
 def GetBytes(data_format):
     return struct.unpack(data_format, classfile.read(struct.calcsize(data_format)))[0]
 
-magic = classfile.read(4)
-if magic != chr(0xCA) + chr(0xFE) + chr(0xBA) + chr(0xBE):
-    print "This is not a class file. Exiting."
-    exit()
+try:
+    classfile = open(args.classfile, 'rb')
+except IOError as e:
+    print "Failed to open class file: %s" % e
+else:
+    magic = classfile.read(4)
+    if magic != chr(0xCA) + chr(0xFE) + chr(0xBA) + chr(0xBE):
+        print "This is not a class file. Exiting."
+        exit()
 
-print "This is a class file."
-minor_version = GetBytes('>H')
-major_version = GetBytes('>H')
+    print "This is a class file."
+    minor_version = GetBytes('>H')
+    major_version = GetBytes('>H')
 
-# The number of entries in the constant_pool plus one
-constant_pool_count = GetBytes('>H')
+    # The number of entries in the constant_pool plus one
+    constant_pool_count = GetBytes('>H')
 
-print "Version: %d.%d" % (major_version, minor_version)
-print "We have %d entries to extract from the constant_pool table (minus one)" % constant_pool_count
+    print "Version: %d.%d" % (major_version, minor_version)
+    print "We have %d entries to extract from the constant_pool table (minus one)" % constant_pool_count
 
-pool_entries_processed = 0
+    pool_entries_processed = 0
 
-constant_pool.append("Unused entry to harmonize our index with Java class index convention.")
+    constant_pool.append("Unused entry to harmonize our index with Java class index convention.")
 
-while pool_entries_processed < constant_pool_count - 1:
-    tag = GetBytes('B')
-    entry = [tag]
+    while pool_entries_processed < constant_pool_count - 1:
+        tag = GetBytes('B')
+        entry = [tag]
 
-    if tag == 1: # Utf8, contains variable-length strings
-        length = GetBytes('>H')
-        string = classfile.read(length)
+        if tag == 1: # Utf8, contains variable-length strings
+            length = GetBytes('>H')
+            string = classfile.read(length)
 
-        entry.append(length)
-        entry.append(string)
-
-    else:
-        for packet in tag_structure[constant_pool_tag[tag]]:
-            data = GetBytes(packet)
-            entry.append(data)
-
-    constant_pool.append(entry)
-    pool_entries_processed += 1
-
-class_access_flags = GetBytes('>H')
-print "Access flags: %04X" % class_access_flags
-
-this_class = GetBytes('>H')
-super_class = GetBytes('>H')
-print "This class is '%s' (%s)" % (Utf8Dereference(this_class), Utf8Dereference(super_class))
-
-interfaces_count = GetBytes('>H')
-print "Direct superinterfaces of this class:" 
-
-counted_interfaces = 0
-while counted_interfaces < interfaces_count:
-    interface = GetBytes('>H')
-    print " #%d: %s" % (counted_interfaces+1, Utf8Dereference(interface))
-    counted_interfaces += 1
-
-fields_count = GetBytes('>H')
-print "Class fields:" 
-
-counted_fields = 0
-while counted_fields < fields_count:
-    access_flags = GetBytes('>H')
-    name_index = GetBytes('>H')
-    name_string = Utf8Dereference(name_index)
-    descriptor_index = GetBytes('>H')
-    attributes_count = GetBytes('>H')
-
-    print " #%d: %s (%d attributes)" % (counted_fields+1, name_string, attributes_count)
-    counted_attributes = 0
-    while counted_attributes < attributes_count:
-        attribute_name_index = GetBytes('>H')
-        attribute_length = GetBytes('>I')
-
-        attribute_content = classfile.read(attribute_length)
-
-        counted_attributes += 1
-
-    counted_fields += 1
-
-exit() 
-methods_count = GetBytes('>H')
-print "There are %d methods." % methods_count
-
-counted_methods = 0
-while counted_methods < methods_count:
-    access_flags = GetBytes('>H')
-    name_index = GetBytes('>H')
-    descriptor_index = GetBytes('>H')
-    attributes_count = GetBytes('>H')
-
-    print "-- %s() has %d attributes." % (Utf8Dereference(name_index), attributes_count)
-    
-    counted_attributes = 0
-    while counted_attributes < attributes_count:
-        attribute_name_index = GetBytes('>H')
-        attribute_length = GetBytes('>I')
-        print "----" + Utf8Dereference(attribute_name_index) + " (%d bytes)" %  attribute_length
-        
-        if Utf8Dereference(attribute_name_index) == "Code":
-            max_stack = GetBytes(">H")
-            max_locals = GetBytes(">H")
-            code_length = GetBytes(">I")
-
-            print "Max stack: %d\nMax locals: %d" % (max_stack, max_locals)
-            print "Code length: %d" % code_length
-
-            code_location = classfile.tell()
-            code_content = classfile.read(code_length)
-            disassemble(code_content, code_location)
-
-            exception_table_length = GetBytes(">H")
-
-            print "Method has %d exceptions" % exception_table_length
-
-            for i in range(0, exception_table_length):
-                start_pc = GetBytes(">H")
-                end_pc = GetBytes(">H")
-                handler_pc = GetBytes(">H")
-                catch_type = GetBytes(">H")
-
-            attributes_count = GetBytes(">H")
-
-            print "Method has %d attributes" % attributes_count
-
-            for i in range(0, attributes_count):
-                attribute_name_index = GetBytes(">H")
-                attribute_length = GetBytes(">I")
-                attributes = classfile.read(attribute_length)
+            entry.append(length)
+            entry.append(string)
 
         else:
+            for packet in tag_structure[constant_pool_tag[tag]]:
+                data = GetBytes(packet)
+                entry.append(data)
+
+        constant_pool.append(entry)
+        pool_entries_processed += 1
+
+    class_access_flags = GetBytes('>H')
+    print "Access flags: %04X" % class_access_flags
+
+    this_class = GetBytes('>H')
+    super_class = GetBytes('>H')
+    print "This class is '%s' (%s)" % (Utf8Dereference(this_class), Utf8Dereference(super_class))
+
+    interfaces_count = GetBytes('>H')
+    print "Direct superinterfaces of this class:" 
+
+    counted_interfaces = 0
+    while counted_interfaces < interfaces_count:
+        interface = GetBytes('>H')
+        print " #%d: %s" % (counted_interfaces+1, Utf8Dereference(interface))
+        counted_interfaces += 1
+
+    fields_count = GetBytes('>H')
+    print "Class fields:" 
+
+    counted_fields = 0
+    while counted_fields < fields_count:
+        access_flags = GetBytes('>H')
+        name_index = GetBytes('>H')
+        name_string = Utf8Dereference(name_index)
+        descriptor_index = GetBytes('>H')
+        attributes_count = GetBytes('>H')
+
+        print " #%d: %s (%d attributes)" % (counted_fields+1, name_string, attributes_count)
+        counted_attributes = 0
+        while counted_attributes < attributes_count:
+            attribute_name_index = GetBytes('>H')
+            attribute_length = GetBytes('>I')
+
             attribute_content = classfile.read(attribute_length)
 
-        counted_attributes += 1
+            counted_attributes += 1
 
-    counted_methods += 1
+        counted_fields += 1
+
+    methods_count = GetBytes('>H')
+    print "There are %d methods." % methods_count
+
+    counted_methods = 0
+    while counted_methods < methods_count:
+        access_flags = GetBytes('>H')
+        name_index = GetBytes('>H')
+        descriptor_index = GetBytes('>H')
+        attributes_count = GetBytes('>H')
+
+        print "-- %s() has %d attributes." % (Utf8Dereference(name_index), attributes_count)
+        
+        counted_attributes = 0
+        while counted_attributes < attributes_count:
+            attribute_name_index = GetBytes('>H')
+            attribute_length = GetBytes('>I')
+            print "----" + Utf8Dereference(attribute_name_index) + " (%d bytes)" %  attribute_length
+            
+            if Utf8Dereference(attribute_name_index) == "Code":
+                max_stack = GetBytes(">H")
+                max_locals = GetBytes(">H")
+                code_length = GetBytes(">I")
+
+                print "Max stack: %d\nMax locals: %d" % (max_stack, max_locals)
+                print "Code length: %d" % code_length
+
+                code_location = classfile.tell()
+                code_content = classfile.read(code_length)
+                disassemble(code_content, code_location)
+
+                exception_table_length = GetBytes(">H")
+
+                print "Method has %d exceptions" % exception_table_length
+
+                for i in range(0, exception_table_length):
+                    start_pc = GetBytes(">H")
+                    end_pc = GetBytes(">H")
+                    handler_pc = GetBytes(">H")
+                    catch_type = GetBytes(">H")
+
+                attributes_count = GetBytes(">H")
+
+                print "Method has %d attributes" % attributes_count
+
+                for i in range(0, attributes_count):
+                    attribute_name_index = GetBytes(">H")
+                    attribute_length = GetBytes(">I")
+                    attributes = classfile.read(attribute_length)
+
+            else:
+                attribute_content = classfile.read(attribute_length)
+
+            counted_attributes += 1
+
+        counted_methods += 1
